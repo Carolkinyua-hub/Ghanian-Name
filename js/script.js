615a612183ef3c73c6a42f6de647ad4512e340fc
carolkinyua-hub <carolnkinyua@gmail.com>
2020-08-24 01:59:42 +0300

    add new form fields

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-let Akan=function akanName(){
-    let d=document.getElementById("dob")
-    let month=d.getmonth()
-    let year=d.getyear()
-    let century=year.slice(0,1)
-    let day =bday.getdate()
+function akanName(){
+    let bday=document.getElementById("dob");
+    console.log(bday) 
+    // let mm=parseInt(d.getMonth());
+    // let yy=parseInt(d.getFullYear());
+    // let cc=parseInt(yy.slice(0,1));
+    // let dd =parseInt(bday.getDate());
+    // let db=Math.round((((cc/4) -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7);
+   
 }
 
 
 
 
+document.write fuge
 
 
 
 
 
-document.getElementById("check").innerHTML="Your Akan name is" +AkanName() +".";
+// document.getElementById("check").innerHTML="Your Akan name is" +AkanName() +".";
